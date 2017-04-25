51a42ef1ce81a0f375d382ae56e6b4e326d23561
Juan Cruz Sosa <nirvguy@gmail.com>
2017-04-25 20:45:40 -0300

    Permite distintos parametros para definir la red

--- practica_3/rn.py
+++ practica_3/rn.py
@@ -2,9 +2,22 @@ import numpy as np
 
 
 class RN:
-    def __init__(self, ns, gs):
-        self.Ws = [ np.random((ns[i+1], ns[i])) for i in range(len(ns)-1) ]
+    random_funct = np.random
+    def __init__(self, gs, **kwargs):
         self.gs = gs
+        if 'Ws' in kwargs:
+            self.Ws = kwargs['Ws']
+        elif 'ns' in kwargs:
+            if 'random_funct' in kwargs:
+                self.random_funct = kwargs['random_funct']
+            self.Ws = [None for i in range(len(ns)-1)]
+            for i in range(len(ns)-1)
+                self.Ws = np.from_function(lambda _, _: self.random_funct(), (ns[i+1],ns[i]))
+        else:
+            raise Exception("Se esperaba las matrices de pesos o la arquitectura")
+
+    def weights(self):
+        return self.Ws
 
     def eval(self, x):
         x = np.array(x).reshape((n, 1))

c3a4aaf2ca420709e69ddfd2d008030fbc8ff84e
Juan Cruz sosa <nirvguy@gmail.com>
2017-04-25 16:46:05 -0300

    Corrige y emprolija Etapa Backward Backpropagation

--- practica_3/rn.py
+++ practica_3/rn.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class RN:
     def __init__(self, ns, gs):
         self.Ws = [ np.random((ns[i+1], ns[i])) for i in range(len(ns)-1) ]
@@ -19,13 +22,29 @@ class RN:
          return vs
 
      def backward(self, vs, y, eta=0.3):
-         deltas = []
-         # g'(h_i(M) * [error])
-         delta = [None for i in range(len(vs))]
-         deltas[-1] = gs[-1][1](vs[-1][0]) * (y - vs[-1][1]))
-         for m in (range(len(vs)-2, 0, -1):
-             for i in range(self.Ws.shape[1]):
-                 delta[i] = gs[m][1](vs[m][0][i]) * sum(z((Ws[m+1][j][i] * deltas[m+1][j])  for j in range(len(self.Ws.shape[0]))))
+        deltas = []
+        # g'(h_i(M) * [error])
+        # W = salidas * entradas
+        # delta = 
+        y = np.array(y).reshape((self.Ws[-1][0], 1))
 
-            delta = gs[m][1](vs[m][0]) * sumatoria (Ws[m+1].T * deltas[m+1])
-            deltas[m] = delta
+        last_layer = -1
+
+        delta = np.zeros((self.Ws[-1].shape[0], 1))
+        for i in range(self.Ws[last_layer].shape):
+            g = gs[last_layer]
+            h = vs[last_layer][0]
+            V = vs[last_layer][1]
+            delta[i] = g.dif(h[i]) * (y - V[i])
+
+        delta_W = eta * delta * self.vs[last_layer-1].T
+        W[last_layer] += delta_W
+
+        for m in (range(len(vs)-2, 0, -1):
+            g = gs[m]
+            h = vs[m][0]
+            for i in range(self.Ws.shape[1]):
+                delta[i] = g.dif(h[i]) * self.Ws.T * delta
+            V = vs[m+1][1]
+            delta_W = eta * delta * V.T
+            W = W + delta_W

eceda28ff530b2ff9c7ce580fd1e293dd695e167
Juan Cruz sosa <nirvguy@gmail.com>
2017-04-25 16:45:19 -0300

    Agrega decorador para vectorizar las funciones de activacion

--- practica_3/af.py
+++ practica_3/af.py
@@ -1,10 +1,20 @@
 import numpy as np
 import math
+
+def vectorize_af(gen_f):
+    def g(*args, **kwargs):
+        f = gen_f(*args, **kwargs)
+        nf = np.vectorize(f)
+        return nf
+    return g
+
+@vectorize_af
 def sign():
     return lambda x: 1 if x >= 0 else -1
 
 def sig(x):
     return 1.0 / (1.0 + math.exp(-x))
 
+@vectorize_af
 def sigmoid(beta=0.5):
     return lambda x: sig(2*beta*x)

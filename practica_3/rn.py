d86ed15a3cf7eda0e519412dc2a01a495673a69b
Juan Cruz Sosa <nirvguy@gmail.com>
2017-04-25 21:01:22 -0300

    Agrega comentarios de los metodos de RN

--- practica_3/rn.py
+++ practica_3/rn.py
@@ -4,6 +4,20 @@ import numpy as np
 class RN:
     random_funct = np.random
     def __init__(self, gs, **kwargs):
+        """ Construye la red neuronal
+
+            gs: Lista de funciones de activación por capa
+            Ws: Si se pasa se inicializa los pesos con las matrices en el
+            ns: Arquitectura de la red.
+            Ejemplo:
+              n = rn.RN(ns=[3,2,1]) es una red con 3 capas
+              (considerando a la de entrada como una capa)
+              que tienen 3, 2, 1 neuronas cada capa respectivamente
+
+            Aclaración: Se deben introducir al menos ns o Ws de lo contrario dara error
+            y si se proveen los dos, solo leera Ws
+        """
+
         self.gs = gs
         if 'Ws' in kwargs:
             self.Ws = kwargs['Ws']
@@ -17,9 +31,13 @@ class RN:
             raise Exception("Se esperaba las matrices de pesos o la arquitectura")
 
     def weights(self):
+        """ Devuelve los pesos """
         return self.Ws
 
     def eval(self, x):
+        """ Alimenta la red neuronal con entrada x
+            y devuelva la salida en formato vector columna"""
         x = np.array(x).reshape((n, 1))
         for W, g in zip(self.Ws, self.gs):
             x = (g(W * x)).T
+        return x

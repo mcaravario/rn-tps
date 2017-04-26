bc07f9d32b47a2a07de412517032686d245683c4
Juan Cruz Sosa <nirvguy@gmail.com>
2017-04-25 22:31:18 -0300

    Arregla identación y cierre de parentesis

--- practica_3/lm.py
+++ practica_3/lm.py
@@ -49,41 +49,41 @@ class BackPropagation(LearningMethod):
             vs.append((h,v))
         return vs
 
-     def backward(self, vs, y, eta=ETA):
-        """ Paso Backward del Backpropagation """
-        ######################################################
-        ##        W en R^(salidas * entradas)               ##
-        ##--------------------------------------------------##
-        ##                                                  ##
-        ##  W_{i,j} = el peso de la entrada j a la salida i ##
-        #####################################################
-        nn = self.neural_network
+    def backward(self, vs, y, eta=ETA):
+       """ Paso Backward del Backpropagation """
+       ######################################################
+       ##        W en R^(salidas * entradas)               ##
+       ##--------------------------------------------------##
+       ##                                                  ##
+       ##  W_{i,j} = el peso de la entrada j a la salida i ##
+       #####################################################
+       nn = self.neural_network
 
-        last_layer = -1
-        # W es la matriz de la ultima capa
-        W = nn.Ws[last_layer]
-        # La ultima capa
-        assert(len(y) == W.shape[0])
-        # interpretamos y como un vector columna
-        y = np.array(y).reshape((W.shape[0], 1))
+       last_layer = -1
+       # W es la matriz de la ultima capa
+       W = nn.Ws[last_layer]
+       # La ultima capa
+       assert(len(y) == W.shape[0])
+       # interpretamos y como un vector columna
+       y = np.array(y).reshape((W.shape[0], 1))
 
-        delta = np.zeros((W.shape[0], 1))
-        g = gs[last_layer]
-        h, v = vs[last_layer]
+       delta = np.zeros((W.shape[0], 1))
+       g = gs[last_layer]
+       h, v = vs[last_layer]
 
-        delta = g.dif(h) * (y - V) # multiplicacion elemento a elemento
-        delta_W = eta * delta * vs[last_layer-1].T
-        W += delta_W
+       delta = g.dif(h) * (y - V) # multiplicacion elemento a elemento
+       delta_W = eta * delta * vs[last_layer-1].T
+       W += delta_W
 
-        for m in (range(len(vs)-2, 0, -1):
-            W = nn.Ws[m]
-            delta = np.zeros((W.shape[0], 1))
-            g = gs[m]
-            h, v = vs[m]
+       for m in range(len(vs)-2, 0, -1):
+           W = nn.Ws[m]
+           delta = np.zeros((W.shape[0], 1))
+           g = gs[m]
+           h, v = vs[m]
 
-            s = W.T * delta
-            # Multiplicacion elemento a elemento
-            delta = g.dif(h) * s
+           s = W.T * delta
+           # Multiplicacion elemento a elemento
+           delta = g.dif(h) * s
 
-            delta_W = eta * delta * v.T
-            W += delta_W
+           delta_W = eta * delta * v.T
+           W += delta_W

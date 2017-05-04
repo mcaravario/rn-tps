c3b9a9ac77f8709113ef6dc95c0328dcf65c7501
Juan Cruz sosa <nirvguy@gmail.com>
2017-05-04 17:04:03 -0300

    Promedia y desvia por columnas

--- tp1/utils.py
+++ tp1/utils.py
@@ -2,6 +2,6 @@ import numpy as np
 
 def get_normalization_function(training):
     xs, ys = zip(*training)
-    avg_xs = np.mean(xs)
-    std_xs = np.std(xs)
+    avg_xs = np.mean(xs, axis=0)
+    std_xs = np.std(xs, axis=0)
     return lambda x: ((x[0] - avg_xs)/std_xs, x[1])

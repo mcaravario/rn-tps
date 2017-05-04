3ea81608cf1e6684fa6be7e4b133d22f8d65dce8
Juan Cruz sosa <nirvguy@gmail.com>
2017-05-04 17:26:31 -0300

    Cambia ej1.py

--- tp1/ej1/ej1.py
+++ tp1/ej1/ej1.py
@@ -24,11 +24,11 @@ def load_training_validation(training_prop=0.6):
 
     training_size = math.floor(training_prop * len(data))
 
-    choices = [1 for i in range(training_size)] + [0 for j in range(len(data)-training_size)]
+    choices = [True for i in range(training_size)] + [False for j in range(len(data)-training_size)]
     random.shuffle(choices)
 
     training = list(compress(data, choices))
-    validation = list(compress(data, map(lambda x: 1-x, choices)))
+    validation = list(compress(data, map(lambda x: not x, choices)))
 
     return training, validation
 
@@ -40,24 +40,21 @@ def porcentaje_aciertos(rn, data):
     return res / len(data)
 
 
-training, validation = load_training_validation(0.8)
+training, validation = load_training_validation(0.6)
 norm_funct = get_normalization_function(training)
 training = list(map(norm_funct, training))
-
-def random_funct(inputs, outputs):
-    r = 4 * math.sqrt(6.0/(inputs+outputs))
-    return random.uniform(-r, r)
+validation = list(map(norm_funct, validation))
 
 def entrenar_nueva_red():
-    # print(training)
-    red = RN(ns=[10, 5, 1], gs=[af.sigmoid(), af.sign()])
+    red = RN(ns=[10, 7, 1], gs=[af.sigmoid(), af.sign()])
     tutor = lm.BackPropagation(red)
     for epoch, error in tutor.learn(training,
                                     training_mode=lm.TrainMode.STOCHASTIC,
-                                    epochs=5000,
-                                    eta=0.02):
-        a = porcentaje_aciertos(red, training)
-        print("{}\t{}\t{}".format(epoch, error, a))
-    print(len(training))
+                                    epochs=1000,
+                                    eta=0.05):
+        error_validation = red.error_training(validation)
+        aciertos_training = porcentaje_aciertos(red, training)
+        aciertos_validation = porcentaje_aciertos(red, validation)
+        print("{}\t{}\t{}\t{}\t{}".format(epoch, error, error_validation, aciertos_training * 100, aciertos_validation * 100))
 
 entrenar_nueva_red()

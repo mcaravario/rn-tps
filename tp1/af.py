0b681e6cd28d5e5b2a822eb1344d775f1b782a41
Juan Cruz Sosa <nirvguy@gmail.com>
2017-05-04 13:15:59 -0300

    Intercambio funciones de math por numpy

--- tp1/af.py
+++ tp1/af.py
@@ -11,15 +11,15 @@ def vectorize_af(gen_f):
 
 @vectorize_af
 def sign():
-    f = lambda x: 1 if x >= 0 else -1
-    f.dif = lambda x: 1
+    f = lambda x: 1.0 if x >= 0 else -1.0
+    f.dif = lambda x: 1.0
     return f
 
 @vectorize_af
 def sigmoid(beta=0.5):
     def f(x):
         try:
-            return 1.0 / (1.0 + math.exp(-2.0 * beta * x))
+            return 1.0 / (1.0 + np.exp(-2.0 * beta * x))
         except OverflowError:
             if x > 0:
                 return 1.0
@@ -29,7 +29,7 @@ def sigmoid(beta=0.5):
 
     def f_dif(x):
         try:
-            y = math.exp(-2.0 * beta * x)
+            y = np.exp(-2.0 * beta * x)
             return 2.0 * beta * y / (1.0 + y)**2
         except OverflowError:
             return 0.0
@@ -40,6 +40,6 @@ def sigmoid(beta=0.5):
 
 @vectorize_af
 def tanh(beta=0.5):
-    f = lambda x: math.tanh(2.0 * beta * x)
+    f = lambda x: np.tanh(2.0 * beta * x)
     f.dif = lambda x: 2.0 * beta / ((2.0 * beta * x)**2 + 1.0)
     return f

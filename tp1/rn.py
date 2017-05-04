62123348192971dcf7bb48dc64c8f727e8e7ec01
Juan Cruz Sosa <nirvguy@gmail.com>
2017-05-04 12:05:27 -0300

    Cambia uniforme en (-1,1) por uniforme en (-0.1,0.1)

--- tp1/rn.py
+++ tp1/rn.py
@@ -3,7 +3,7 @@ import random
 
 
 class RN:
-    random_funct = lambda inputs, outputs: random.uniform(-1.0, 1.0)
+    random_funct = lambda inputs, outputs: random.uniform(-0.1, 0.1)
     def __init__(self, gs, **kwargs):
         """ Construye la red neuronal
 

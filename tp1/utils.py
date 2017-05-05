e069843bc6ea62e292706bcd23c770ea7f1c2881
Juan Cruz Sosa <nirvguy@gmail.com>
2017-05-05 13:12:43 -0300

    Soluciona normalizacion cuando la varianza es cero

--- tp1/utils.py
+++ tp1/utils.py
@@ -4,4 +4,12 @@ def get_normalization_function(training):
     xs, ys = zip(*training)
     avg_xs = np.mean(xs, axis=0)
     std_xs = np.std(xs, axis=0)
-    return lambda x: ((x[0] - avg_xs)/std_xs, x[1])
+    def t(x):
+        r = [None for i in range(len(x[0]))]
+        for i in range(len(x[0])):
+            if std_xs[i] != 0:
+                r[i] = (x[0][i] - avg_xs[i])/std_xs[i]
+            else:
+                r[i] = 0
+        return (r, x[1])
+    return t

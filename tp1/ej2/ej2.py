c51170701b136a02534e19394393c4a1cd31727e
Juan Cruz Sosa <nirvguy@gmail.com>
2017-05-07 20:04:12 -0300

    Actualiza ej2.py

--- tp1/ej2/ej2.py
+++ tp1/ej2/ej2.py
@@ -37,21 +37,42 @@ norm_funct = get_normalization_function(training)
 training = list(map(norm_funct, training))
 validation = list(map(norm_funct, training))
 
-def random_funct(inputs, outputs):
-    r = 4 * math.sqrt(6.0/(inputs+outputs))
-    return random.uniform(-r, r)
+DIR_EJ2_BASE='tp1/ej2/pruebas/'
 
-def entrenar_nueva_red():
-    # print(training)
-	print(len(training))
-	print(len(validation))
-    red = RN(ns=[9, 20, 1], gs=[af.tanh(), af.identity()])
-    tutor = lm.BackPropagation(red)
-    for epoch, error in tutor.learn(training,
-                                    training_mode=lm.TrainMode.STOCHASTIC,
-                                    epochs=1000,
-                                    eta=0.02):
-        print("{}\t{}".format(epoch, error))
-    print(len(training))
+# Experimento 1: Variamos capas y cantidad de neuronas
+redes_1 = (RN(ns=[10, 1], gs=[af.sign()]),)
+
+experimento_1 = {'nombre': 'experimento 1',
+                 'redes': redes_1,
+                 'parametros':[{'lc':lm.BackPropagation, 'learn_params':{'eta':0.03, 'epochs':500, 'training_mode': lm.TrainMode.STOCHASTIC}}]}
+
+
+experimentos = [experimento_1]
+
+def experimentar(i, j):
+    experimento = experimentos[i]
+    params = experimento['parametros'][j]
+    learn_params = params['learn_params']
+    print(";".join(map(str, learn_params.items())))
+    for red in experimento['redes']:
+        print(red.ns)
+        tutor = params['lc'](red)
+        filename_base = DIR_EJ2_BASE
+        filename_base += 'ex_{}-{}_red_{}'.format(str(i+1),str(j+1),"-".join(map(str, red.ns)))
+        with open(filename_base + '.dat', 'w+') as f:
+            for epoch, error_training in tutor.learn(training,
+                                                     **learn_params):
+                error_validation = red.error_training(validation)
+                print("{}\t{}\t{}".format(epoch,
+                                          error_training,
+                                          error_validation)
+                     file=f)
+        f.close()
+
+
+nr_experimento = int(sys.argv[1])
+nr_param =  int(sys.argv[2])
+experimentar(nr_experimento, nr_param)
 
 entrenar_nueva_red()
+

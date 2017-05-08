f644b24710b47f25af58b33d190f9e14ce2d311d
Juan Cruz Sosa <nirvguy@gmail.com>
2017-05-08 11:24:59 -0300

    Actualiza build_table.sh

--- tp1/ej1/build_table.sh
+++ tp1/ej1/build_table.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-echo "\\begin{matrix}[c|c|c]"
+echo "\\begin{matrix}[c|c|c|c|c]"
+echo -e "\tArchivo & Mejor % de aciertos training & Mejor % de validacion & Mejor % de validacion \\\\"
 for filename in $@; do
-	max_training=`cat "$filename" | awk 'NR == 1 {line = $0; max = $4}
-		                   NR > 1 && $4 > max {line = $0; max = $4}
-		                   END{print line}' | awk '{print $4,"("$1,"epochs)"}'`
-	max_validation=`cat "$filename" | awk 'NR == 1 {line = $0; max = $5}
-		                   NR > 1 && $5 > max {line = $0; max = $5}
-		                   END{print line}' | awk '{print $5,"("$1,"epochs)"}'`
-	echo -e "$filename & ${max_training} & ${max_validation} \\\\"
+	max_training=`cat "$filename" | sort -k4r | head -n 1 | awk '{print $4,"("$1,"epochs)"}'`
+	max_validation=`cat "$filename" | sort -k7 | sort --stable -k5r | head -n 1 | awk '{print $5,"("$1,"epochs) & ",$6,"&",$7;}'`
+	echo -e "\t$filename & ${max_training} & ${max_validation} \\\\"
 done
 echo "\\end{matrix}"

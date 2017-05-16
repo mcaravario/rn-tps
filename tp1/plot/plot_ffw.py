5afecf9be776c43ba37ebb4a7601c192f38b149e
Juan Cruz Sosa <nirvguy@gmail.com>
2017-05-16 00:55:47 -0300

    Vuelve a subir los graficos de las redes pero png

--- tp1/plot/plot_ffw.py
+++ tp1/plot/plot_ffw.py
@@ -1,14 +1,22 @@
 import sys
 import argparse
-from graphviz import Graph
+import pydot
 
 def draw(dot, lista):
+    nodes = dict()
     for l in range(len(lista)):
         for i in range(lista[l]):
-            dot.node('X_'+str(l)+'_'+str(i), "", color='blue')
+            name = 'X_'+str(l)+'_'+str(i)
+            node = pydot.Node(name,label="", color='blue', style="")
+            nodes[name] = node
+            dot.add_node(node)
+            # dot.node('X_'+str(l)+'_'+str(i), "", color='blue')
             if l > 0:
                 for j in range(lista[l-1]):
-                    dot.edge('X_'+str(l)+'_'+str(i), 'X_'+str(l-1)+'_'+str(j))
+                    na = nodes['X_'+str(l)+'_'+str(i)]
+                    nb = nodes['X_'+str(l-1)+'_'+str(j)]
+                    edge = pydot.Edge(na, nb)
+                    dot.add_edge(edge)
 
 parser = argparse.ArgumentParser("Plotea red feed-foward")
 parser.add_argument('input_units', metavar='U', type=int, nargs=1, help="Cantidad de entradas")
@@ -16,8 +24,8 @@ parser.add_argument('hidden_units', metavar='U', type=int, nargs='*', help="Cant
 parser.add_argument('output_units', metavar='U', type=int, nargs=1, help="Cantidad de salidas")
 args = parser.parse_args()
 
-dot = Graph(comment='Graph')
+dot = pydot.Dot(graph_type='graph')
 architecture = args.input_units + args.hidden_units + args.output_units
 print(architecture)
 draw(dot, architecture)
-dot.render("-".join(map(str,architecture))+".svg", view=True)
+dot.write_png("-".join(map(str,architecture))+".png")

7977140a6bee015ed405aa57ef73ced6b688c1cb
Juan Cruz Sosa <nirvguy@gmail.com>
2017-06-17 15:29:59 -0300

    Implementa Sanger matricialmente

--- tp2/trainer.py
+++ tp2/trainer.py
@@ -32,11 +32,13 @@ class Sanger(Trainer):
         self.rn = rn
 
     def fit(self, x, eta):
-        y_predict = self.rn.eval(x)
-        delta = np.zeros(self.rn.w.shape)
+        outputs = self.rn.w.shape[0]
+        x = np.array(x).reshape((self.rn.n, 1))
 
-        for j in range(delta.shape[0]):
-            for i in range(delta.shape[1]):
-                delta[j][i] = eta * y_predict[j][0] * (x[i] - sum(self.rn.w[k][i] * y_predict[k][0] for k in range(0,j)))
+        y_predict = self.rn.eval(x)
+
+        triangular = np.triu(np.ones((outputs, outputs)))
+        delta = x - np.dot(self.rn.w.T, np.multiply(triangular, y_predict))
+        delta = eta * (np.multiply(delta, y_predict.T)).T
 
         self.rn.w += delta

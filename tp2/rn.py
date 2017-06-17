e7e456b3fc152a5f838e4ed1e9862936d2a2b781
Juan Cruz Sosa <nirvguy@gmail.com>
2017-06-17 17:40:09 -0300

    Agrega Modelo Red Neuronal SOA

--- tp2/rn.py
+++ tp2/rn.py
@@ -17,3 +17,27 @@ class RN:
 
         # Realiza el proceso sinaptico
         return np.dot(self.w, x)
+
+class SOM:
+    def __init__(self, inputs, rows, columns):
+        self.inputs = inputs
+        self.rows = rows
+        self.columns = columns
+        self.w = npr.random((rows, columns, inputs))
+
+    def dist(self, i, j, x):
+        return np.linalg.norm(self.w[i][j]-x)
+
+    def winner(self, x):
+        x = np.array(x).flatten()
+        min_i = min_j = 0
+        min_dist = self.dist(min_i, min_j, x)
+
+        for i in range(self.rows):
+            for j in range(self.columns):
+                d = self.dist(i, j, x)
+                if min_dist > d:
+                    min_i = i
+                    min_j = j
+                    min_dist = d
+        return min_i, min_j

ac2a97bd0ba0fdf80c31f07cb383482de4d51bad
Juan Cruz Sosa <nirvguy@gmail.com>
2017-06-22 16:19:43 -0300

    Elimina espacios y comentarios que no iban

--- tp2/rn.py
+++ tp2/rn.py
@@ -6,16 +6,13 @@ import abc
 from numpy import random as npr
 
 class RN:
-
     def __init__(self, inputs, outputs):
         self.n = inputs
         self.w = npr.random((outputs, self.n))
 
     def eval(self, x):
-        # Si la entrada esta no viene con bias
         x = np.array(x).reshape((self.n, 1))
 
-        # Realiza el proceso sinaptico
         return np.dot(self.w, x)
 
 class SOM:

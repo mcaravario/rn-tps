665848bfd95fa457b6d3cc5c99bc2ee625e2d9ec
Juan Cruz Sosa <nirvguy@gmail.com>
2017-06-23 13:31:46 -0300

    ej2: Agrega opcion para preprocesar las entradas

--- tp2/ej2.py
+++ tp2/ej2.py
@@ -3,8 +3,8 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from rn import SOM
-from trainer import SOMTrainer
+from rn import RN, SOM
+from trainer import Oja, Sanger, SOMTrainer
 import operator
 from graficar import graficar_som
 import argparse
@@ -13,6 +13,8 @@ import config
 def parser_args():
     parser = argparse.ArgumentParser(description='Entrena una red neuronal auto organizada')
     parser.add_argument('--db', type=str, help="Base de entrenamiento CSV", default=config.DB_TRAINING)
+    parser.add_argument('-p', '--preprocess', help="Preprocesa la entrada usando Aprendizaje Hebiano", dest='preprocess', action='store_true')
+    parser.add_argument('--no-preprocess', help="Usa la entrada original", dest='preprocess', action='store_false')
     parser.add_argument('-e', '--epochs', help="Cantidad de epocas", type=str, default=50)
     parser.add_argument('--test-size', help="Proporcion de test en la particion entrenamiento/test", type=float, default=0.3)
     parser.add_argument('-r', '--rows', help='Filas en la grilla SOM', type=int, default=10)
@@ -21,6 +23,7 @@ def parser_args():
     parser.add_argument('--sigma0', help="Sima inicial", type=float, default=10)
     parser.add_argument('--tao0', help="Factor dilatacion del tiempo (eta)", type=float, default=20)
     parser.add_argument('--tao1', help="Factor dilatacion del tiempo (sigma)", type=float, default=20)
+    parser.set_defaults(preprocess=False)
     return parser.parse_args()
 
 def main():
@@ -34,6 +37,28 @@ def main():
 
     training = x_train.as_matrix()
     tests = x_test.as_matrix()
+
+    if args.preprocess:
+        inputs = x_train.shape[1]
+        outputs = 9
+        red = RN(inputs, outputs)
+        trainer = Sanger(red)
+
+        trainer.fit_train(training, epochs=500, eta=0.0001)
+
+        results = []
+        for x in training:
+            y = red.eval(x).flatten()
+            results.append(y)
+
+        results_test = []
+        for x in tests:
+            y = red.eval(x).flatten()
+            results_test.append(y)
+
+        training = np.array(results)
+        tests = np.array(results_test)
+        print("Preprocess finished")
 
 
     inputs = training.shape[1]

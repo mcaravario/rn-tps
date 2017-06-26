14b8fd61c8616720f604c961154c92fd053164ba
Juan Cruz Sosa <nirvguy@gmail.com>
2017-06-26 20:05:22 -0300

    Barra de color discreta Oja y Sanger

--- tp2/graficar.py
+++ tp2/graficar.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.cm as cm
@@ -16,14 +17,15 @@ def graficar_componentes(matriz, y, matriz_test, y_test, rule, projection=1):
 
     POINT_SIZE = 10
 
-    colors = cm.rainbow(np.linspace(0, 1, 9))
-    y2 = list(map(lambda x: colors[x-1], y))
-    z2 = list(map(lambda x: colors[x-1], y_test))
+    cmap = plt.get_cmap('jet', 9)
+
+    y2 = list(map(lambda x: x, y))
+    z2 = list(map(lambda x: x, y_test))
 
     fig = plt.figure()
     ax1 = fig.add_subplot(131, projection='3d')
-    ax1.scatter(c1[:,[0]], c1[:,[1]], c1[:,[2]], s=POINT_SIZE, facecolors=y2, marker='o')
-    ax1.scatter(v1[:,[0]], v1[:,[1]], v1[:,[2]], s=POINT_SIZE, facecolors=z2, marker='^')
+    ax1.scatter(c1[:,[0]], c1[:,[1]], c1[:,[2]],vmin=1,vmax=9,cmap=cmap, s=POINT_SIZE, c=y2, marker='o')
+    ax1.scatter(v1[:,[0]], v1[:,[1]], v1[:,[2]],vmin=1,vmax=9,cmap=cmap, s=POINT_SIZE, c=z2, marker='^')
 
     ax1.set_title("Componentes 1 a 3")
     ax1.set_xlabel('X')
@@ -32,8 +34,8 @@ def graficar_componentes(matriz, y, matriz_test, y_test, rule, projection=1):
 
     ax2 = fig.add_subplot(132, projection='3d')
     ax2.set_title("Componentes 4 a 6")
-    ax2.scatter(c2[:,[0]], c2[:,[1]], c2[:,[2]], s=POINT_SIZE, facecolors=y2, marker='o')
-    ax2.scatter(v2[:,[0]], v2[:,[1]], v2[:,[2]], s=POINT_SIZE, facecolors=z2, marker='^')
+    ax2.scatter(c2[:,[0]], c2[:,[1]], c2[:,[2]],vmin=1,vmax=9,cmap=cmap, s=POINT_SIZE, c=y2, marker='o')
+    ax2.scatter(v2[:,[0]], v2[:,[1]], v2[:,[2]],vmin=1,vmax=9,cmap=cmap, s=POINT_SIZE, c=z2, marker='^')
 
     ax2.set_xlabel('X')
     ax2.set_ylabel('Y')
@@ -42,8 +44,10 @@ def graficar_componentes(matriz, y, matriz_test, y_test, rule, projection=1):
     ax3 = fig.add_subplot(133, projection='3d')
 
     ax3.set_title("Componentes 7 a 9")
-    ax3.scatter(c3[:,[0]], c3[:,[1]], c3[:,[2]], s=POINT_SIZE, facecolors=y2, marker='o')
-    ax3.scatter(v3[:,[0]], v3[:,[1]], v3[:,[2]], s=POINT_SIZE, facecolors=z2, marker='^')
+    i = ax3.scatter(c3[:,[0]], c3[:,[1]], c3[:,[2]],vmin=1,vmax=9,cmap=cmap, s=POINT_SIZE, c=y2,  marker='o')
+    j = ax3.scatter(v3[:,[0]], v3[:,[1]], v3[:,[2]],vmin=1,vmax=9,cmap=cmap, s=POINT_SIZE, c=z2,  marker='^')
+    cbar_ax3 = fig.add_axes()
+    plt.colorbar(i, cax=cbar_ax3, extend='min')
 
     ax3.set_xlabel('X')
     ax3.set_ylabel('Y')
@@ -64,6 +68,7 @@ def graficar_componentes(matriz, y, matriz_test, y_test, rule, projection=1):
         ax3.view_init(elev=10., azim=135)
 
     plt.suptitle("Reduccion con {}".format(rule.capitalize()))
+    plt.tight_layout()
     fig.savefig('componentes_{}_{}.png'.format(rule,projection))
 
 def graficar_som(grilla_t, grilla_v, title, filename):
